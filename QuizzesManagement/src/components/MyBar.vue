85c3e6bb186f19d136d950510dd98bfd2ebd9772
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-10-08 22:54:05 +0700

    Test Navbar

--- QuizzesManagement/src/components/MyBar.vue
+++ QuizzesManagement/src/components/MyBar.vue
@@ -1,6 +1,9 @@
 <template lang="">
-    <div>
-        
+    <div class="w3-bar w3-black">
+        <ul>
+            <li class="w3-bar-item w3-hover-red"><a href="#home">Home</a></li>
+            <li class="w3-bar-item w3-hover-green"><a href="#about">About</a></li>
+        </ul>
     </div>
 </template>
 
@@ -9,6 +12,28 @@ export default {
     
 }
 </script>
-<style lang="">
-    
+<style scoped>
+    ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
 </style>
